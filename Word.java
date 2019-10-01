8e1695c6d4f8216f3d97641439dd7a58f06e0bc1
jstephensasij <45579926+jstephensasij@users.noreply.github.com>
2019-10-01 17:29:23 +0900

    Add files via upload

---
+++ Word.java
@@ -0,0 +1,12 @@
+public class Word
+{
+   /**
+      Tests whether a letter is a vowel
+      @param letter a string of length 1
+      @return true if letter is a vowel
+    */
+   public boolean isVowel(String letter)
+   {
+      return false;
+   }
+}
